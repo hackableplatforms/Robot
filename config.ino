13c3ab3d7ff2e430b7a3d46f390ddf9b432b2490
Hackable Platforms <hackableplatforms@gmail.com>
2026-01-12 14:46:57 +1100

    Create config.ino

---
+++ config.ino
@@ -0,0 +1,25 @@
+void setup() {
+  //LEDs
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(10, OUTPUT);
+
+  //Buzzer
+  pinMode(11, OUTPUT);
+}
+
+
+void loop() {
+
+}
